304c6cffe00c2e144e6408dfbb5a36060e8071e1
Mariia Horiunova <mariya.vivchar@gmail.com>
2024-10-26 23:56:29 +0300

    find-elements-tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from ("cypress");
+import { defineConfig } from 'cypress';
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // настройки для плагинов
     },
+    supportFile: false, // если support file не требуется
   },
 });
